55353798326b61a2f66fb60f710061a1f481e6f7
ajo-823 <alexisjorrick@gmail.com>
2023-10-31 11:52:46 +0000

    added second arg supptype to function

--- data_function.R
+++ data_function.R
@@ -1,7 +1,12 @@
 # function to remove na values from dataset
+# also returns row of longest tooth in given supptype
+# designed for r dataset "ToothGrowth"
 
-remove_na <- function(data) {
+longest_tooth <- function(data,supptype) {
+  library("tidyverse")
   data_transformed <- data %>%
-    drop_na()
+    drop_na() %>%
+    filter(supp == supptype) %>%
+    filter(len == max(len)) %>%
     return(data_transformed)
 }
